a260cec9d8daa80df399c1966cdc1f05deaf6009
ags2121 <alexsilva88@gmail.com>
2016-09-29 09:08:11 -0400

    do not poll server

--- src/clj/personal_space_site/server.clj
+++ src/clj/personal_space_site/server.clj
@@ -6,20 +6,9 @@
             [ring.middleware.logger :refer [wrap-with-logger]]
             [environ.core :refer [env]]
             [ring.adapter.jetty :refer [run-jetty]]
-            [clojure.java.io :as io]
-            [org.httpkit.client :as httpkit]
-            [overtone.at-at :refer :all])
+            [clojure.java.io :as io])
 
   (:gen-class))
-
-(def my-pool (mk-pool))
-
-(defn get-requests []
-  (httpkit/get "http://www.personalspacetheband.com" (fn [_] (println "server polled"))))
-
-;; every half hour poll the server
-(defn poll-server []
-  (every 1800000 get-requests my-pool))
 
 (defroutes routes
            (GET "/" _
@@ -36,5 +25,4 @@
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 10555))]
-    (run-jetty http-handler {:port port :join? false})
-    (poll-server)))
+    (run-jetty http-handler {:port port :join? false})))
